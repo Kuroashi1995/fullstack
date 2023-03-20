e839903edc594044d5369293e2d4c5203a5cb8b2
Kuroashi1995 <kurashi.prog@gmail.com>
2023-03-20 13:19:51 -0300

    reactive reviews implemented

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const Statistics = ({ reviews }) => {
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <ul>
+        <li>good {reviews.good}</li>
+        <li>neutral {reviews.neutral}</li>
+        <li>bad {reviews.bad}</li>
+      </ul>
+    </div>
+  );
+};
+
+const GiveFeedback = ({ handleClick }) => {
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={() => handleClick("good")}>good</button>
+      <button onClick={() => handleClick("neutral")}>neutral</button>
+      <button onClick={() => handleClick("bad")}>bad</button>
+    </div>
+  );
+};
 
 function App() {
+  const [reviews, setReviews] = useState({ good: 0, neutral: 0, bad: 0 });
+  const handleReviews = (category) => {
+    let newReviews;
+    switch (category) {
+      case "good":
+        newReviews = { ...reviews, good: reviews.good + 1 };
+        setReviews(newReviews);
+        break;
+      case "neutral":
+        newReviews = { ...reviews, neutral: reviews.neutral + 1 };
+        setReviews(newReviews);
+        break;
+      case "bad":
+        newReviews = { ...reviews, bad: reviews.bad + 1 };
+        setReviews(newReviews);
+        break;
+      default:
+        newReviews = reviews;
+        setReviews(newReviews);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GiveFeedback handleClick={handleReviews} />
+      <Statistics reviews={reviews} />
     </div>
   );
 }
