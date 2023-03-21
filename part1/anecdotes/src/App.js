6e6fabc6648c4ab8829df6b2746e3b4f34ea4390
Kuroashi1995 <kurashi.prog@gmail.com>
2023-03-21 13:42:02 -0300

    implemented random different anecdote

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const GenerateButton = ({ callback, anecdoteNumber }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={() => callback(anecdoteNumber)}>Next Anecdote</button>
+  );
+};
+function App() {
+  const [anecdoteNumber, setAnecdoteNumber] = useState(0);
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+  const generateRandomNumber = (anecdoteNumber) => {
+    let number;
+    do {
+      number = Math.floor(Math.random() * 8);
+    } while (number === anecdoteNumber);
+    console.log(number, anecdoteNumber);
+    setAnecdoteNumber(number);
+  };
+  return (
+    <div>
+      <p>{anecdotes[anecdoteNumber]}</p>
+      <GenerateButton
+        callback={generateRandomNumber}
+        anecdoteNumber={anecdoteNumber}
+      />
     </div>
   );
 }
