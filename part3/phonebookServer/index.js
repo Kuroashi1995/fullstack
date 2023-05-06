1aceadc63afb02fde2152b039305cdf2c7253269
Kuroashi1995 <kurashi.prog@gmail.com>
2023-05-06 12:24:45 -0400

    add data and simple routes

---
+++ part3/phonebookServer/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+
+const sumEntries = () => {
+  return data.length;
+};
+
+data = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (request, response) => {
+  response.json(data);
+});
+
+app.get("/info", (request, response) => {
+  response.send(
+    `<div><p>This phonebook cointains information about ${
+      data.length
+    } people</p> <p>${Date(Date.now())}</p></div>`
+  );
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
