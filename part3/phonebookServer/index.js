c5e056e41ad01f6cc34ee8570cfaffc06412f35d
Kuroashi1995 <kurashi.prog@gmail.com>
2023-05-10 23:15:51 -0400

    configure morgan to show request body

--- part3/phonebookServer/index.js
+++ part3/phonebookServer/index.js
@@ -13,9 +13,25 @@ const unknownEndpoint = (request, response, next) => {
     error: "unknown endpoint",
   });
 };
+morgan.token("body", (req, res) => {
+  return `${JSON.stringify(req.body)}`;
+});
 
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      tokens.body(req, res),
+    ].join(" ");
+  })
+);
 
 data = [
   {
