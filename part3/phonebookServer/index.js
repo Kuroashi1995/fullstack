3d21ec46e37757fe062208e9be2a551b8cfa8164
Kuroashi1995 <kurashi.prog@gmail.com>
2023-06-20 09:06:39 -0400

    eslint config

--- part3/phonebookServer/index.js
+++ part3/phonebookServer/index.js
@@ -4,40 +4,40 @@ const {
   findContact,
   deleteContact,
   updateContact,
-} = require("./services/Database");
-const express = require("express");
-var morgan = require("morgan");
-const app = express();
-const cors = require("cors");
+} = require("./services/Database")
+const express = require("express")
+var morgan = require("morgan")
+const app = express()
+const cors = require("cors")
 
 const errorHandler = (error, request, response, next) => {
-  console.error("se corrio error handler", error.name);
+  console.error("se corrio error handler", error.name)
 
   if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
+    return response.status(400).send({ error: "malformatted id" })
   } else if (error.name === "ValidationError") {
-    return response.status(400).send({ error: "missing or incorrect content" });
+    return response.status(400).send({ error: "missing or incorrect content" })
   }
 
-  next(error);
-};
+  next(error)
+}
 
 //middlewares
 const unknownEndpoint = (error, request, response, next) => {
   if (error) {
-    next(error);
+    next(error)
   }
   response.status(404).send({
     error: "unknown endpoint",
-  });
-};
+  })
+}
 morgan.token("body", (req, res) => {
-  return `${JSON.stringify(req.body)}`;
-});
+  return `${JSON.stringify(req.body)}`
+})
 
-app.use(cors());
-app.use(express.static("build"));
-app.use(express.json());
+app.use(cors())
+app.use(express.static("build"))
+app.use(express.json())
 app.use(
   morgan((tokens, req, res) => {
     return [
@@ -49,90 +49,89 @@ app.use(
       tokens["response-time"](req, res),
       "ms",
       tokens.body(req, res),
-    ].join(" ");
+    ].join(" ")
   })
-);
+)
 
 app.get("/", (request, response) => {
-  response.send("<h1> Welcome to my Phonebook </h1>");
-});
+  response.send("<h1> Welcome to my Phonebook </h1>")
+})
 app.get("/api/persons", async (request, response, next) => {
   try {
-    response.json(await getContacts());
+    response.json(await getContacts())
   } catch (error) {
-    (error) => next(error);
+    (error) => next(error)
   }
-});
+})
 
 app.put("/api/persons/:id", async (request, response, next) => {
-  console.log(`Request body: ${JSON.stringify(request.body)}`);
+  console.log(`Request body: ${JSON.stringify(request.body)}`)
   const contactArg = {
     name: request.body.name,
     phone: request.body.phone,
-  };
-  try {
-    contact = await updateContact(request.params.id, contactArg);
-    contact ? response.json(contact) : response.status(404).end();
-  } catch (error) {
-    next(error);
   }
-});
+  try {
+    contact = await updateContact(request.params.id, contactArg)
+    contact ? response.json(contact) : response.status(404).end()
+  } catch (error) {
+    next(error)
+  }
+})
 
 app.get("/api/persons/:id", async (request, response, next) => {
   try {
-    contact = await findContact(request.params.id);
-    contact ? response.json(contact) : response.status(404).end();
+    contact = await findContact(request.params.id)
+    contact ? response.json(contact) : response.status(404).end()
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 app.get("/info", async (request, response, next) => {
   try {
-    contacts = await getContacts();
+    contacts = await getContacts()
     response.send(
-      `<div><p>This phonebook cointains information about ${
-        contacts.length
+      `<div><p>This phonebook cointains information about ${contacts.length
       } people</p> <p>${Date(Date.now())}</p></div>`
-    );
+    )
   } catch (error) {
-    (error) => next(error);
+    (error) => next(error)
   }
-});
+})
 
 app.delete("/api/persons/:id", async (request, response, next) => {
   try {
-    deletion = await deleteContact(request.params.id);
-    deletion ? response.status(204).end() : response.status(404).end();
+    deletion = await deleteContact(request.params.id)
+    deletion ? response.status(204).end() : response.status(404).end()
   } catch (error) {
-    (error) => next(error);
+    (error) => next(error)
   }
-});
+})
 
 app.post("/api/persons", async (request, response, next) => {
-  const body = request.body;
+  const body = request.body
   try {
-    const contacts = await getContacts();
+    const contacts = await getContacts()
     if (contacts.find((contact) => contact.name === body.name)) {
       response.status(400).json({
         error: "name must be unique",
-      });
+      })
     }
     const contact = {
       name: body.name,
       phone: body.phone,
-    };
-    await addContact(contact);
-    response.json(contact);
+    }
+    await addContact(contact)
+    response.json(contact)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-app.use(unknownEndpoint);
-app.use(errorHandler);
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
